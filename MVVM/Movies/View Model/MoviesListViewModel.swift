fa0f466009c9d389ba8b093ea9e4aa9d9caf979c
Abdulrahman Alaa <abdulrhmanalaa114@gmail.com>
2022-04-05 02:21:08 +0200

    frist commit

--- MVVM/Movies/View Model/MoviesListViewModel.swift
+++ MVVM/Movies/View Model/MoviesListViewModel.swift
@@ -6,3 +6,55 @@
 //
 
 import Foundation
+import RxCocoa
+import RxSwift
+import Alamofire
+
+class MoviesListViewModel {
+    
+    var loadingBehavior = BehaviorRelay<Bool>(value: false)
+    
+    private var isTableHidden = BehaviorRelay<Bool>(value: false)
+    private var moviesModelSubject = PublishSubject<[Movie]>()
+    
+    var moviesModelObservable: Observable<[Movie]> {
+        return moviesModelSubject
+    }
+    var isTableHiddenObservable: Observable<Bool> {
+        return isTableHidden.asObservable()
+    }
+    
+    var title = "Movies"
+    
+    func viewDidLoad(){
+        getData()
+    }
+    
+    func getData() {
+        loadingBehavior.accept(true)
+ 
+        let api: MoviesAPIProtocol = MoviesAPI()
+        api.getMovies { [weak self](result) in
+            guard let self = self else {return}
+            self.loadingBehavior.accept(false)
+            
+            switch result {
+                
+            case .success(let response):
+
+                self.moviesModelSubject.onNext(response ?? [])
+                break
+            case .failure(let error):
+                print(error.localizedDescription)
+                break
+            }
+        }
+        
+        
+        
+        
+    }
+    
+    
+   
+}
--- MVVM/Movies/Views/MoviesListViewController.swift
+++ MVVM/Movies/Views/MoviesListViewController.swift
@@ -6,24 +6,72 @@
 //
 
 import UIKit
+import RxCocoa
+import RxSwift
 
 class MoviesListViewController: UIViewController {
-
+    
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var moviesContainerView: UIView!
+    var viewModel: MoviesListViewModel!
+    let disposeBag = DisposeBag()
+    
+    
+    let movieTableViewCell = "MovieTableViewCell"
+   
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        self.navigationController?.navigationBar.isHidden = false
+        self.title = viewModel.title
+        
+        subscribeToMoviesSelection()
+        setupTableView()
+        bindToHiddenTable()
+        subscribeToLoading()
+        subscribeToResponse()
+        getMovies()
+        
+    }
+    func setupTableView() {
+        tableView.register(UINib(nibName: movieTableViewCell, bundle: nil), forCellReuseIdentifier: movieTableViewCell)
+    }
+    func bindToHiddenTable() {
+        viewModel.isTableHiddenObservable.bind(to: self.moviesContainerView.rx.isHidden).disposed(by: disposeBag)
+    }
+    func subscribeToLoading() {
+        viewModel.loadingBehavior.subscribe(onNext: { (isLoading) in
+            if isLoading {
+                self.showIndicator(withTitle: "", and: "")
+            } else {
+                self.hideIndicator()
+            }
+        }).disposed(by: disposeBag)
+    }
+    func subscribeToResponse() {
+        self.viewModel.moviesModelObservable
+            .bind(to: self.tableView
+                .rx
+                .items(cellIdentifier: movieTableViewCell,
+                       cellType: MovieTableViewCell.self)) { row, movie, cell in
+                        print(row)
+                        cell.textLabel?.text = movie.title
+        }
+        .disposed(by: disposeBag)
+    }
+    func subscribeToMoviesSelection() {
+        Observable
+            .zip(tableView.rx.itemSelected, tableView.rx.modelSelected(Movie.self))
+            .bind {  selectedIndex, movie in
+                
+                print(selectedIndex, movie.title )
+        }
+        .disposed(by: disposeBag)
+    }
+    func getMovies() {
+        viewModel.getData()
     }
 
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
